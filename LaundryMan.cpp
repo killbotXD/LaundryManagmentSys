c796c6a762fa7b877dc06338f6f428d67b4bffc8
sahu6 <48767743+sahu6@users.noreply.github.com>
2019-11-21 01:43:57 +0530

    Basic template ready for 3 users

---
+++ LaundryMan.cpp
@@ -0,0 +1,114 @@
+#include <fstream>
+#include<conio.h>
+#include<stdio.h>
+#include<string.h>
+#include<math.h>
+using namespace std;
+int input(){
+int i;
+do{
+int is;      
+fflush(stdin);
+i=scanf("%d",&is);
+if(i<1)
+{
+    printf("please enter a valid value...\nEnter again :");
+}
+else
+{
+    return is;
+}
+}while(i==0);
+   return 0;
+}
+
+
+int main()
+{
+    fstream f;
+     int x;
+    // travel t;
+do{
+//clrscr();
+printf("**********************Welcome**********************\n");
+printf("Enter your type...\n");
+printf("1)ADMIN\n");
+printf("2)LAUNDRY BOY\n");
+printf("3)CUSTOMER\n");
+printf("4)EXIT\n");
+x=input();
+switch(x){
+    case 1: //ADMIN
+            printf("\n");
+            printf("Are you a new User?\nEnter 1 for yes" or no\n");
+            x=input();
+            swith(x){
+                case 1: printf("Sign up");
+                //enter code here
+                case 0: printf("Login");
+                //enter code here
+            }
+    case 2: //Laundry BOY
+            printf("Enter choice...\n");
+            printf("1)Display Schedule\n");
+            printf("2)Add Laundry\n");
+            printf("3)Update Profile\n");
+            printf("4)Exit\n");
+            x=input();
+            switch(x){
+                case 1: printf("Present Schedule is as follows :");
+                //Enter code here
+                case 2: printf("Enter choice...\n");
+                        printf("1)Add Item\n");
+                        printf("2)Exit \n");
+                        y=input();
+                        switch(y){
+                            case 1: printf("Choose Item to added\n");
+                                    printf("1)Shrit\n"); 
+                                    printf("2)T-Shrit\n");
+                                    printf("3)Jeans\n");
+                                    printf("4)Trouser\n");
+                                    printf("5)Bed sheet\n");
+                                    printf("6)Towel\n");
+                                    printf("7)Suit\n");
+                                    printf("8)Lower\n");
+                                    printf("9)Shorts\n");
+                                    printf("10)Exit\n");
+                                    z=input();
+                                    switch(z){
+                                        case 1:
+                                        case 2:
+                                        case 3:
+                                        case 4:
+                                        case 5:
+                                        case 6:
+                                        case 7:
+                                        case 8:
+                                        case 9:printf("Enter quantity to be added\n");
+                                        q=input();
+                                        printf("%d items of type %d added to laundry",q,z);
+                                        printf("Do you want to add more?\n");
+                                        ch=input();
+                                        if(ch==1)
+                                        //should go back to outer switch...I don't know how :(
+                                        if(ch==0)
+                                        break;
+                                        
+                                    case 2: break;
+                                    }    
+                            case 2: break;
+                        }
+                case 3: printf("Update");
+                //Enter code here
+                case 4: break;
+            }
+            
+    case 3: //CUSTOMER
+    default: printf("Thanks for visiting");
+
+
+}//switch
+}while(x!=4);//do
+
+
+}
