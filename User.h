aaf756d91b4ae1f988608465bcd0c4fd2eb2b39f
killbotxd <sonawane.1@iitj.ac.in>
2019-11-22 19:48:53 +0530

    update user class

--- User.h
+++ User.h
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<fstream>
 #include<stdio.h>
 #include<string.h>
 using namespace std;
@@ -8,6 +9,12 @@ string username;
 string password;
 string id;
 public:
+User(){
+    username="Username";
+    password="password";
+    id="NULL";
+}
+
 string returnId(){
     return id;
 }
@@ -20,15 +27,34 @@ int authenticate(string temp){
 }
 //you need to authenticate in the function that you are going to call befoe you pass a new name
 // or password to this method
+/* char *pa=new char[203];    int access2;
+
+                      pa=t.enterpass();
+                      access2=t.access(pa);
+                      if(access2==1){gotoxy(15,18);cout<<"access granted.....press enter..... ";getch();
+gotoxy(15,19);        t.changepass();}*/
 void changePassword(string newPwd){
     password=newPwd;
 }
 void changeName(string newName){
     username=newName;
 }
+void changeId(string newId){
+    id=newId;
+}
 void getSchedule(){
     cout<<"Need to add this shit";
 }
-
+void addUser(string newId){
+    id=newId;
+    string tempUsername,tempPwd;
+    cout<<"Enter Username: ";
+    cin>>tempUsername;
+    changeName(tempUsername);
+    cout<<"Enter Password(note: default password is\"password\"):";
+    cin>>tempPwd;
+    changePassword(tempPwd);
+    
+}
 
 };
