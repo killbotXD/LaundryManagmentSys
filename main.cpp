dbabcf7cd8d3822f1f75b43f7a518756e51760f7
killbotxd <sonawane.1@iitj.ac.in>
2019-11-23 18:59:01 +0530

    add login feature and show admin menu

--- main.cpp
+++ main.cpp
@@ -9,8 +9,11 @@
 
 using namespace std;
 
- 
+Admin a;
 
+#define ent {cout<<"press enter..."<<endl; getch();}
+#define fr(a) f.read((char*)&a,sizeof(a))
+#define skg  f.seekg(0)
 void gotoxy(int x,int y)	
 {
 	printf("%c[%d;%df",0x1B,y,x);
@@ -37,7 +40,38 @@ else
    return 0;
 }
 
-
+void showAdminMenu(){
+    int inp;
+   do{
+     clrscr();  
+    cout<<"*****************ADMIN MENU************"<<endl;
+    cout<<"1)Update Schedulle"<<endl;
+    cout<<"2)Add New Admin"<<endl;
+    cout<<"3)View Schedule"<<endl;
+    cout<<"4)Register Laundry Personell"<<endl;
+    cout<<"5)Settings"<<endl;
+    cout<<"6)Exit"<<endl;
+    inp=input();
+    switch(inp){
+        case 1:{cout<<"Update Schedulle"<<endl;ent
+            break;
+        }
+        case 2:{  cout<<"Add New Admin"<<endl;ent
+            break;
+            }
+        case 3:{cout<<"View Schedule"<<endl;ent
+            break;
+            }
+        case 4:{cout<<"Register Laundry Personell"<<endl;ent
+            break;
+            }
+        case 5:{cout<<"Settings"<<endl;ent
+            break;
+            }
+        case 6: return;
+    }
+   }while(inp!=6);
+}
 int main()
 {
     fstream f;
@@ -59,7 +93,7 @@ cout<<"Enter any one of the choices: ";
 x=input();
 switch(x){
     case 1:{ //ADMIN
-            Admin a;
+           
             f.open("Admins.dat",ios::out|ios::app);f.close();
             char ch;
             printf("\n");
@@ -67,7 +101,7 @@ switch(x){
             cin>>ch;
             switch(ch){
                 
-                case 'y':{clrscr(); printf("********************SIGN UP**********************");
+                case 'y':{clrscr(); printf("********************SIGN UP*******************");
                         cout<<endl<<"enther the Id of the new user :";
                         string newId;
                         cin>>newId;
@@ -95,10 +129,47 @@ switch(x){
                         }
                           cout<<endl<<"press enter...";  
                         getch();
-                        //TODO: need to add the admin method which will be called to show the menu of admin 
+                        showAdminMenu();
                         break;}
                 
-                case 'n': {printf("Login");
+                case 'n': {
+                    string tempId,Id,tempPwd;
+                    printf("********************Login********************\n");
+                        cout<<"Enter your User ID: ";
+                        cin>>Id;
+                        f.open("Admins.dat",ios::in);
+                        skg;
+                        int flag=0;
+                        while(!f.eof()){
+                            fr(a);
+                            if(f.eof())break;
+                            tempId=a.returnId();   
+                            if(tempId==Id){cout<<"User found!!"<<endl;flag++;break;}
+                        }
+                        int flag2=0;
+                         while(1){
+                            if(flag==0){
+                                cout<<"User Not Found"<<endl;break;
+                                }
+                            else if(flag!=0&&flag2!=3){
+                                cout<<"Enter password: ";
+                                cin>>tempPwd;
+                                if(a.authenticate(tempPwd)){
+                                    cout<<"Welcome "<<a.returnName()<<"!"<<endl;
+                                    showAdminMenu();
+                                    break;
+                                }
+                                else{
+                                    flag2++;
+                                    cout<<"Password is incorrect you have "<<3-flag2<< "chances left!!"<<endl;
+                                }
+                            }
+                            else{
+                                break;
+                            }       
+                         }      
+                         cout<<"press enter....";         
+                        getch();
                  //TODO: need to add the admin method which will be called to show the menu of admin after login
                         break;}
                
