19d76a8839a4558160e7ba2d4a36068a5619eb50
killbotxd <sonawane.1@iitj.ac.in>
2019-11-25 15:22:27 +0530

    add update schedule functionality

--- main.cpp
+++ main.cpp
@@ -15,6 +15,57 @@ void is_firsttime_launch(){
     }
     
 }
+int lboyExists(string id){
+    fstream flboy;
+                lboy b;
+                //is_firsttime_launch_lboy();
+                flboy.open("lboy.dat",ios::in);
+                flboy.seekg(0);
+                while(!flboy.eof()){
+                    flboy.read((char*)&b,sizeof(b));
+                    //cout<<a.id<<" id "<<tempid<<endl;
+                    if(strcmp(b.id,id.c_str())==0){flboy.close();return 1;}
+
+                }
+                flboy.close();
+                return 0;
+                
+}
+void addScheduleItem(){
+fstream faddSI;
+scheduleitem schItem;
+string tempDay,tempAdd,tempId;
+string tempLboyId[20];
+cout<<"Enter Day: ";
+cin>>tempDay;
+cout<<"Enter Address of the Scheduled Laundry: ";
+cin>>tempAdd;
+string answer="yes";
+int i=0;
+while(answer=="y"||answer=="yes"||answer=="Y"||answer=="YES"){
+    cout<<"Enter Laundry Boy Id to Be assigned: ";
+    cin>>tempId;
+    if(lboyExists(tempId)){tempLboyId[i]=tempId;i++;}
+    else{cout<<"The Laundry boy does not exist!!!"<<endl<<endl;}
+    cout<<"Do you want to add another laundry Boy?";
+    cin>>answer;
+}
+
+cout<<endl<<"Confirm add Schedule Item?";
+cin>>answer;
+if(answer=="y"||answer=="yes"||answer=="Y"||answer=="YES"){
+    strcpy(schItem.day,tempDay.c_str());
+    for(i=0;i<sizeof(tempLboyId)/sizeof(string);i++){
+        strcpy(schItem.lboyId[i],tempLboyId[i].c_str());
+    }
+    strcpy(schItem.add,tempAdd.c_str());
+    faddSI.open("schedule.dat",ios::out|ios::app);
+    faddSI.write((char*)&schItem,sizeof(schItem));
+    faddSI.close();
+}
+
+
+}
 void updateSchedule(){
     int inpUpdateSchedule;
     do{
@@ -28,13 +79,16 @@ void updateSchedule(){
         cin>>inpUpdateSchedule;
         switch (inpUpdateSchedule)
         {
-        case 1:{cout<<"Add"; ent 
+        case 1:{cout<<"Add"; 
+           addScheduleItem();
+        
+            ent 
             break;}
         case 2: {cout<<"Edit"; ent
             break;}
         case 3: {cout<<"Delete"; ent
             break;}
-        case 4:{cout<<"Exit";}
+        case 4:{cout<<"Exit"<<endl;}
         }
     }while(inpUpdateSchedule!=4);
 }
